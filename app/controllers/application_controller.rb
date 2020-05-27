d108b75e03ff33764bf504a6f6ac79f4273c244b
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-27 00:06:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    @user = User.new(:username => params[:username], :password => params[:password])
-    if user.save 
-      redirect "/account"
+    if params[:username] =="" || params[:password] == "" 
+      redirect '/failure'
     else 
-      redirect "/failure"
-    end
+      User.create(username: params[:username], password)
 
   end
 
