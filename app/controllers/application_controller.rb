c44be650e7b50c0f9c33cdeca9cbc9fc2c83c313
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-27 00:16:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     @user = User.find_by(username: params[:username])
     if @user && user.authenticate(params[:password]) == @user.id  
-      redirect  "/account"
+      redirect  '/account'
     else 
       redirect  "/failure"
     end
